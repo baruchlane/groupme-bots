90699adc98bb1fd15d46f6b752842f6b2f603eea
Me 242623 <242623@hebrewbirthdays.nfshost.com>
2015-11-29 06:58:12 +0000

    more restructring

--- bots/shabbos.php
+++ bots/shabbos.php
@@ -6,6 +6,10 @@ function __autoload($class_name)
     require_once 'src/' . $class_name . '.php';
 }
 
-echo 'shabbos.php';
-//$shabbosBot = new GroupMeService('shabbos');
-//$shabbosBot->sendRawMessage('test');
+$params = json_decode(file_get_contents("php://input"), true);
+file_put_contents('logs/dev.log', json_encode($params) . PHP_EOL, 
+FILE_APPEND | LOCK_EX);
+$shabbosBot = new GroupMeService('shabbos');
+if ($params['sender_type'] != 'bot') {
+    $shabbosBot->sendRawMessage('pong');
+}
